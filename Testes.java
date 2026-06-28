37b941a9b3c59641a6a76a0b512ba5f1fca2a10e
Rafael Quirino <faelquirino3@gmail.com>
2026-06-28 10:38:51 -0300

    Update Testes.java
    Added distribution method calls to both hash tables.

--- Testes.java
+++ Testes.java
@@ -4,20 +4,21 @@ import static com.mycompany.hashtable.TabelaHashLista.carregarDicionario;
 
 public class Testes {
         public static void main(String[] args) {
+
         int capacidadeTabela = 100000;
-
-        System.out.println("TESTES\n");
-
+            
         TabelaHashLista tabela1 = new TabelaHashLista(capacidadeTabela);
-        tabela1.metodo = 1;
+        tabela1.metodo = 1; 
         carregarDicionario(tabela1);
-        System.out.println("Colisões - Metodo de Multiplicação: " + tabela1.colisoes );
-
+        System.out.println("Colisoes - Metodo de Multiplicacao: " + tabela1.colisoes);
+        tabela1.distribuicao();
         System.out.println("");
-        
         TabelaHashLista tabela2 = new TabelaHashLista(capacidadeTabela);
-        tabela2.metodo = 2;
+        tabela2.metodo = 2; 
         carregarDicionario(tabela2);
-        System.out.println("Colisões - Algoritmo DJB2: " + tabela2.colisoes);
-    }
+        System.out.println("Colisoes - Algoritmo DJB2: " + tabela2.colisoes);
+        tabela2.distribuicao();
+        }
+        
 }
+
